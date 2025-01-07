42c7761049575192ac0ef524d19e7bd045d70cb4
cig-jacquesthurling <j.thurling@capital-sa.com>
2025-01-07 14:54:13 +0200

    Updated the values

--- src/main.cs
+++ src/main.cs
@@ -1,10 +1,13 @@
 using System.Net;
 using System.Net.Sockets;
 
-// Uncomment this line to pass the first stage
-Console.Write("$ ");
-
 // Wait for user input
-string? userInput = Console.ReadLine();
+while (true)
+{
+    // Uncomment this line to pass the first stage
+    Console.Write("$ ");
+    
+    string? userInput = Console.ReadLine();
 
-Console.WriteLine($"{userInput}: command not found");
+    Console.WriteLine($"{userInput}: command not found");    
+}
